c0d4ff70b71f8cab778ae071573d6865bcf9f66e
Lhc_fl <lhcfl@outlook.com>
2025-03-20 19:18:56 +0800

    cmm add token

--- Code/main.c
+++ Code/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("done;\n");
+    printf("start;\n");
     yyparse();
+    printf("end;\n");
 }
--- Code/predefines.h
+++ Code/predefines.h
@@ -8,4 +8,6 @@ extern int fileno(FILE*);
 
 void yyerror(char* msg) { fprintf(stderr, "error: %s\n", msg); }
 
+void cmm_log_token(char* ty, char* yytext) { printf("[Token { name: \"%s\", val: \"%s\" }] ", ty, yytext); }
+
 #endif
