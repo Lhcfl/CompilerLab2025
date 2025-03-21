7c3598a6cebfeee4437a0f4d242506636b4e8110
Lhc_fl <lhcfl@outlook.com>
2025-03-21 11:46:41 +0800

    submit

---
+++ make-zip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 定义 zip 文件名
+ZIP_NAME="submit.zip"
+
+# 需要排除的文件/文件夹
+EXCLUDE_LIST=(
+    ".git"
+)
+
+# 组合排除参数
+EXCLUDE_ARGS=()
+for item in "${EXCLUDE_LIST[@]}"; do
+    EXCLUDE_ARGS+=("-x" "$item/*" "-x" "$item")
+done
+
+echo "Remove zip"
+rm *.zip
+
+# 执行 zip 命令
+zip -r "$ZIP_NAME" ./* "${EXCLUDE_ARGS[@]}"
+
+# 检查是否成功
+if [ $? -eq 0 ]; then
+    echo "打包成功: $ZIP_NAME"
+else
+    echo "打包失败！"
+    exit 1
+fi
